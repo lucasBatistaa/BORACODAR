1856b0d593019880e9bc829616802efa7fb50dac
Lucas Batista <lucas.lucas2004.30@gmail.com>
2023-02-22 22:15:08 -0300

    Desafio 07

--- bloco_carnaval/main.js
+++ bloco_carnaval/main.js
@@ -1,4 +1,7 @@
 var cardsSection = document.querySelector("#cards")
+var buttonMap = document.querySelector("#button-map")
+var buttonList = document.querySelector("#button-list")
+var locationOptionsDiv = document.querySelector("#location")
 
 fetch("settings.json").then((response) => {
     response.json().then((data) => {
@@ -7,7 +10,7 @@ fetch("settings.json").then((response) => {
             `
             <div class="card">
                 <img src=${block.image} alt="imagem">
-
+        
                 <div class="card-info">
                     <h3>${block.name}</h3>
                     <p>${block.description}</p>
@@ -20,5 +23,34 @@ fetch("settings.json").then((response) => {
             </div>
             `
         })
+
+        data.options.map((option, i) => {
+            if (i == 0) {
+                locationOptionsDiv.innerHTML += 
+                `<option value=${i} selected>${option}</option>`
+            } else {
+                locationOptionsDiv.innerHTML += 
+                `<option value=${i}>${option}</option>`
+            }    
+        })
     })
 })
+
+function toggleClassesButtons() {
+    buttonMap.classList.toggle('secondary')
+    buttonMap.classList.toggle('primary')
+    buttonList.classList.toggle('secondary')
+    buttonList.classList.toggle('primary')
+}
+
+function showMap() {
+    if (buttonMap.classList.contains('secondary')) {
+        toggleClassesButtons()
+    }
+}
+
+function showList() {
+    if (buttonList.classList.contains('secondary')) {
+        toggleClassesButtons()
+    }
+}
